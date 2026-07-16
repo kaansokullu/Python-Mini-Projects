2e4f75b88e07c085f9800109fb99027d9ef4f8d5
Kaan Sokullu <kaansokullu06@gmail.com>
2026-07-16 15:32:46 +0300

    Two folder were created and named as Games and Tools. Previous two game projects were inserted into the games folder. Two new project, Caesar-Cipher and Password Generator, were added to the tools folder.README file was updated.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# Mini-Games
-A repository for the mini games that I made for practicing.
+# Mini Python Projects
+
+A collection of projects written in Python.
+
+This repository contains simple terminal-based projects that I developed to improve my Python programming skills. New small games and tools will be added as I learn more about programming.
+
+## Contents
+
+### 🎮 Games
+
+| Project | Description |
+|------|------|
+| Rock Paper Scissors | A classic rock, paper and scissors game against the computer |
+| Hangman | Guessing the hidden word with limited lifes |
+
+### 🛠 Tools
+
+| Project | Description |
+|------|------|
+| Password Generator | A program that creates a password according to your demand |
+| Caesar Cipher | A program that helps you encrypt your messages and allows you to decrypt received messages |
---
+++ tools/Caesar-Cipher/main.py
@@ -0,0 +1,42 @@
+import art
+
+print(art.logo)
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def encrypt(plain_text, shift_amount):
+    cipher_text = ""
+    for char in plain_text:
+        alphabet_index = (alphabet.index(char) + shift_amount) % 26
+        cipher_text += alphabet[alphabet_index]
+
+    print("\n-----RESULT-----")
+    print(f"The encrypted text is: {cipher_text}\n")
+
+def decrypt(plain_text, shift_amount):
+    cipher_text = ""
+    for letter in plain_text:
+        shifted_position = alphabet.index(letter) - shift_amount
+        shifted_position %= len(alphabet)
+        cipher_text += alphabet[shifted_position]
+
+    print("\n-----RESULT-----")
+    print(f"The decoded text is: {cipher_text}\n")
+
+def cipher_caesar(plain_text, shift_amount, e_or_d):
+    if e_or_d == "e":
+        return encrypt(plain_text, shift_amount)
+    else:
+        return decrypt(plain_text, shift_amount)
+    
+while True:
+    encode_or_decode = input("Type 'e' to encrypt, type 'd' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    cipher_caesar(text, shift, encode_or_decode)
+
+    run_again = input("Type 'y' if you want to continue, otherwise 'n': ").lower()
+
+    if run_again == "n":
+        break
