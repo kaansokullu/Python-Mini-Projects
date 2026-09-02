926a82f5d37539b15544fb169b15a9cc2242115a
Kaan Sokullu <kaansokullu06@gmail.com>
2026-09-02 16:43:58 +0300

    feat(find-all-cities): add player input and check function to the game

--- games/Find-All-Cities/main.py
+++ games/Find-All-Cities/main.py
@@ -1,8 +1,39 @@
 import turtle
+import pandas as pd
 
 screen = turtle.Screen()
 screen.setup(width=986, height=510)
 
 screen.bgpic("blank_turkey_map.png")
+
+df = pd.read_csv("turkey_cities.csv")
+df["condition"] = False
+
+def data_check(player_input):
+    if player_input in df["city"].values and not df.loc[df["city"] == player_input, "condition"].values[0]:
+        df.loc[df["city"] == player_input, "condition"] = True
+        return True
+    else:
+        return False
+
+def mark_city(city_name):
+    city_data = df[df.city == city_name]
+    x_cor = int(city_data.X.iloc[0])
+    y_cor = int(city_data.Y.iloc[0])
+    turtle.hideturtle()
+    turtle.penup()
+    turtle.teleport(x_cor, y_cor)
+    turtle.write(city_name, font=("Times New Roman", 13, "normal"))
+
+while True:
+    player_answer = screen.textinput(title="Guess the City", prompt="Write a city name:").title()
+
+    if player_answer == "Istanbul":
+        player_answer = "İstanbul"
+    elif player_answer == "Izmir":
+        player_answer = "İzmir"
+
+    if data_check(player_answer):
+        mark_city(player_answer)
 
 screen.exitonclick()
